a138b6dd06c3a60c653ca35d76c9275e4e809832
Nicolas KEITA <nicolas.keita@epitech.eu>
2019-03-29 06:17:59 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='my-trading-bot',
+    version='0.1dev',
+    packages=['bot', ],
+    license='MIT',
+    long_description=open('README.md').read(),
+)
